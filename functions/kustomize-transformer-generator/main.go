9d8ca1747172e9db347d865dfe3de1f47880d98b
Luka Skugor <luka.skugor@chimeramail.com>
2024-11-02 15:15:29 +0100

    Initial commit

---
+++ functions/kustomize-transformer-generator/main.go
@@ -0,0 +1,104 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"maps"
+	"os"
+	"path"
+	"path/filepath"
+	"slices"
+
+	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
+	"sigs.k8s.io/kustomize/api/konfig"
+	"sigs.k8s.io/kustomize/api/types"
+)
+
+func fileNameAnnotation(fileName string) string {
+	return fmt.Sprintf("file.kustomize.kuberik.io/%s", fileName)
+}
+
+func generator(rl *fn.ResourceList) (bool, error) {
+	resourcesDir := os.Getenv("RESOURCES_DIR")
+	if resourcesDir == "" {
+		resourcesDir = "/tmp"
+	}
+	kustomizationPath, _, err := rl.FunctionConfig.NestedString("path")
+	if err != nil {
+		return false, err
+	}
+	files, err := filepath.Glob(path.Join(resourcesDir, kustomizationPath, "*"))
+	if err != nil {
+		return false, err
+	}
+	fileAnnotations := make(map[string]string)
+	kustomizationFile := ""
+	for _, file := range files {
+		fileName := filepath.Base(file)
+		if slices.Contains(konfig.RecognizedKustomizationFileNames(), fileName) {
+			if kustomizationFile != "" {
+				return false, fmt.Errorf("multiple kustomization files found in %s", kustomizationPath)
+			}
+			kustomizationFile = file
+		}
+		contents, err := os.ReadFile(file)
+		if err != nil {
+			return false, err
+		}
+		fileAnnotations[fileNameAnnotation(fileName)] = string(contents)
+	}
+	if kustomizationFile == "" {
+		return false, fmt.Errorf("kustomization file not found in %s", kustomizationPath)
+	}
+	kustomization := &types.Kustomization{}
+	if err := kustomization.Unmarshal([]byte(fileAnnotations[fileNameAnnotation(filepath.Base(kustomizationFile))])); err != nil {
+		return false, err
+	}
+	delete(fileAnnotations, fileNameAnnotation(filepath.Base(kustomizationFile)))
+
+	// set type meta
+	if kustomization.TypeMeta.Kind == "" {
+		kustomization.TypeMeta.Kind = "Kustomization"
+	}
+	if kustomization.TypeMeta.APIVersion == "" {
+		kustomization.TypeMeta.APIVersion = "kustomize.config.k8s.io/v1beta1"
+	}
+
+	// set name
+	if kustomization.MetaData == nil {
+		kustomization.MetaData = &types.ObjectMeta{}
+	}
+	kustomization.MetaData.Name = rl.FunctionConfig.GetName()
+
+	// set annotations
+	if kustomization.MetaData.Annotations == nil {
+		kustomization.MetaData.Annotations = make(map[string]string)
+	}
+	maps.Copy(kustomization.MetaData.Annotations, fileAnnotations)
+
+	generatedKustomizationTransformer, err := fn.NewFromTypedObject(kustomization)
+	if err != nil {
+		return false, err
+	}
+	rl.Items = append(rl.Items, generatedKustomizationTransformer)
+	return true, nil
+}
+
+func main() {
+	if err := fn.AsMain(fn.ResourceListProcessorFunc(generator)); err != nil {
+		os.Exit(1)
+	}
+}
